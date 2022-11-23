cda1326e5f30cf0c6ad90d2f494bafec21b05b41
Ednael Ednael <ednael_11@hotmail.com>
2022-11-23 18:41:57 -0300

    23.11

--- notebooks/remove_itens.py
+++ notebooks/remove_itens.py
@@ -3,3 +3,9 @@ def define_obra(data):
     result = data.Descrição.str.contains("Manutenção|manutenção|Manutenções|manutenções|Automotiva|AUTOMOTIVO|AUTOMOTIVA|MANUTENÇÃO|automotiva|MANUTENCAO|REFORMA|reformas", na=False)
     
     return (result.replace({True:'remove',False:'-'}))
+
+def remove_from_objeto(data):
+    
+    result = data.Objeto.str.contains("Manutenção|manutenção|Manutenções|manutenções|Automotiva|AUTOMOTIVO|AUTOMOTIVA|MANUTENÇÃO|automotiva|MANUTENCAO|REFORMA|reformas", na=False)
+    
+    return (result.replace({True:'remove',False:'-'}))
